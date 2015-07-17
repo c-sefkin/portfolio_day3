07cf543ac992c51ea0c1773458546c4563075e41
Chris Sefkin <c.sefkin@gmail.com>
2015-07-17 14:28:22 -0700

    more styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,30 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
-//=require bootstrap-sprockets
 //= require_tree .
+
+var ready;
+ready = function() {
+  "use strict";
+
+  $("#refs-show").click(function() {
+    $(".references").slideDown( 450, function() {
+      $("#refs-hide").show();
+    });
+    $(this).hide();
+  });
+  $("#refs-hide").click(function() {
+    $(".references").slideUp( 350, function() {
+      $("#refs-show").show();
+    });
+    $(this).hide();
+  });
+
+  // set bootstrap 'active' class on the nav link for the current route.
+  var routeParts = window.location.pathname.split('/');
+  $('ul.nav li a[href="/' + routeParts[1] + '"]').parent("li").addClass("active");
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
